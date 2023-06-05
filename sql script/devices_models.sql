1488fdfdfc6d8a26d4c45d552d16c74456c91539
Ismat-Samadov <ismetsemedov@gmail.com>
2023-06-05 14:52:28 +0400

    check

--- sql script/devices_models.sql
+++ sql script/devices_models.sql
@@ -8,3 +8,9 @@ group by trim(replace(replace(replace(replace(replace(substr(a.user_agent, instr
           order by say desc fetch first 10 rows only;
 
           ----select count(distinct pin ) say from dwh.d_customer ;
+
+		  
+select a.type, count(distinct a.user_id) as say
+  FROM birbank.d_customer_device a
+ WHERE state = 'ACTIVE'
+ group by a.type;
