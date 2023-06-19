584c8b6d73517b6cb5798f8cfdf803c8bc95b33e
Ismat-Samadov <ismetsemedov@gmail.com>
2023-06-19 10:54:28 +0400

    check

--- sql script/prolongation_depozit.sql
+++ sql script/prolongation_depozit.sql
@@ -1,5 +1,13 @@
-select unique a.contract_ref_no,
-              a.prolongation_count,
-       case when sysdate > a.maturity_date then 'close' else 'open' end as activity
+select unique 
+       a.pin,
+       a.cif,
+       a.contract_ref_no,
+       a.prolongation_count,
+       case
+         when sysdate > a.maturity_date then
+          'close'
+         else
+          'open'
+       end as activity
   from front.dm_deposit_portfolio a
  order by PROLONGATION_COUNT desc;
