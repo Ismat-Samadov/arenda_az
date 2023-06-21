c570697a221d55ffdc4971558cce73c0265e6acf
Ismat-Samadov <ismetsemedov@gmail.com>
2023-06-21 16:19:11 +0400

    check

--- sql script/attach_cards.sql
+++ sql script/attach_cards.sql
@@ -1,13 +1,4 @@
-select *
-  from birbank.d_verified_attach_card a
- inner join cms.d_card b
-    on a.product_id = b.CARDUID
- inner join birbank.d_customers_new c
-    on a.user_id = c.user_id
- where c.cif = '298324';
-
------------------------------------------------------------
 select * from birbank.d_verified_attach_card a 
 inner join birbank.d_customers_new b  on a.user_id = b.user_id 
 inner join cms.d_card c on a.product_id = c.CARDUID 
-where b.cif = '8930610';
+where b.cif = '7500850';
