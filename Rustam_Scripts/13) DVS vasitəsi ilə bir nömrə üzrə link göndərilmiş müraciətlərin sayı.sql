bba33a6d3906bbce9fb703bf18c8827522708169
Ismat <51861011+Ismat-Samadov@users.noreply.github.com>
2023-08-09 17:38:06 +0400

    Delete Rustam_Scripts directory

--- Rustam_Scripts/13) DVS vasitəsi ilə bir nömrə üzrə link göndərilmiş müraciətlərin sayı.sql
+++
@@ -1,7 +0,0 @@
-select count (distinct  A.CIF) as say , A.MOBILE,B.SALES_CHANNEL
-from cx.d_dvs_customer A
-left join cx.f_dvs_order B on A.ID=B.CUSTOMER_ID 
-left join optimus.d_contract O on o.cif = a.cif 
-where o.cash_contract_startdate between '01-Apr-22' and '30-Apr-22' and b.sales_channel = 'teleSales' --and a.mobile like '%5606263'
-group by a.mobile,  b.sales_channel
-order by say desc  ;
--- Rustam_Scripts/31) IVR vasitəsi ilə təyin olunan 3DS nömrələrin bir şəxsə aid olması.sql
+++
@@ -1,9 +0,0 @@
-select distinct fc.phone_number, t.value_date,t.idclient, t.accountnoone, cc.address, cc.createsysdate,
-       case when fc.phone_number = substr(cc.address, 4) then 'okay' else 'not okay' end as result
-from cms.f_transaction_details t
-join cms.d_card_cns cc on cc.objectuid= t.objectuid and cc.createdate = t.value_date
-join callcenter.f_ivr_report_data fc on fc.cms_id = t.idclient and t.value_date = trunc ( call_start_date ) 
-where t.value_date between date '2022-09-01' and date '2022-09-30' 
-and concat(t.device,t.entcode) = 'F06'
-and lower(term) like '%vr%'
-and fc.forwarded_to_operator = '0'; 
