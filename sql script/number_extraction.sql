eb3b97b0201ab0593dd096224be16e362854ac19
Ismat <51861011+Ismat-Samadov@users.noreply.github.com>
2023-08-09 17:37:08 +0400

    Delete sql script directory

--- sql script/number_extraction.sql
+++
@@ -1,9 +0,0 @@
-select /*+ paralel*/
-w.userdata,
-substr(regexp_replace(w.userdata, '[^0-9]', ''), 1, 12) nums,
-w.*
-  from cms.f_transaction_details w
-where w.value_date > '01-Jun-2023'
-   and w.termcode = '49831'
-   and w.device = 'F'
-   and w.entcode = '06';
